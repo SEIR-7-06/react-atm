511b538becfb8e503c713d43f60e6a98b670f587
off2mxrs <off2mxrs@gmail.com>
2021-08-27 16:54:26 -0700

    Savings Acoount created within same Class

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 // Custom Components
 import Account from './Account';
-import Savings from './Savings';
+
 
 // Resources
 import logo from './ga.png';
@@ -17,7 +17,8 @@ function App() {
         <div id="title">Bank of GA</div>
       </div>
       <Account name="Checking" />
-      <Savings name="savings" />
+      <Account name="Savings" />
+     
       <div className="clear"></div>
     </div>
   );
